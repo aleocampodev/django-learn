45611ee813d4ca19ea33139dd496e3f4d3ff9778
Aleja Ocampo <omaira.527@hotmail.com>
2024-06-19 20:00:04 -0500

    add params

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
+def index(request):
+    return HttpResponse("<p>Hello</p>")
+
+def hello(request, username):
+    print(username)
     return HttpResponse("<h2>Hello World</h2>")
 
 def about(request):
